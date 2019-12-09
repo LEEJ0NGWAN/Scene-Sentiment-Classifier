190a7db397e08b191769dab05a14a188fc07f35c
lhwanstar <47805785+lhwanstar@users.noreply.github.com>
2019-12-09 23:16:32 +0900

    [tools] optical flow example

---
+++ data/movieDataExtractor.py
@@ -0,0 +1,195 @@
+from random import randint as rand
+import cv2 as cv
+import math
+import re
+import os
+import numpy as np
+
+# resize 크기
+width   = 480
+height  = 270
+
+# 영화 장르
+vid_type        = ['action', 'horror', 'romance', 'ani2D', 'ani3D', 'sf']
+
+# 각 구간의 시작, 끝 프레임과 길이를 저장할 리스트
+range_list      = []
+
+# 각 구간에서 뽑을 프레임의 개수 리스트
+randSize_list   = []
+
+# 각 구간에서 랜덤으로 뽑은 프레임을 나열한 리스트
+randFrame_list  = []
+
+# 입력 받는 시간대 hh:mm:ss를 hh, mm, ss로 끊는 놈
+timeSplitter    = re.compile('[0-6][0-9]')
+
+# 저장할 파일의 시작 넘버링
+file_num        = 1
+
+# 몇 프레임을 연속으로 저장할 건지
+shot_length     = 2
+
+# 뽑을 프레임 수
+extraction_size = 600
+random_limit    = math.ceil(extraction_size/shot_length)
+
+# 동영상 불러오기
+while True:
+    print("장르 : ", vid_type)
+    typeName    = input("장르 선택 (ex: horror) >>> ")
+
+    if typeName not in vid_type:
+        print("입력 오류")
+        continue
+
+    fileName    = input("동영상 이름 (ex: test.avi) >>>")
+
+    try:
+        vid  = cv.VideoCapture(fileName)
+        vid.set(cv.CAP_PROP_FPS, 24)
+        if not vid.isOpened(): raise NameError('동영상이 아니거나 파일이 없음')
+
+    except cv.error     as e : print(e)
+    except Exception    as e : print(e)
+
+    else: break
+
+# 입력 동영상 총 프레임 수
+total_length    = int(vid.get(cv.CAP_PROP_FRAME_COUNT))
+fps             = vid.get(cv.CAP_PROP_FPS)
+
+# 프레임 뽑을 구간의 개수 정하기
+while True:
+    try:
+        count = int(input("뽑을 구간의 개수 (ex: 2) >>>"))
+    
+    except ValueError   : print("공백ㄴㄴ 소수ㄴㄴ 양의 정수만 가능")
+    if count < 1        : print("최소 1개여야함")
+
+    else: break
+
+while True:
+    accumulated_length = 0
+    range_list.clear()
+    # 각 구간의 시작, 끝 시간 입력
+    for index in range(count):
+        while True:
+            
+            print("\n%d번째 구간"%index)
+
+            inputTime   = input("시작 시간 입력 (ex: 00:12:53) >>>")
+            start_time  = timeSplitter.findall(inputTime)
+
+            if not len(start_time) == 3:
+                print("제대로 입력하셈 hh:mm:ss 맞춰야함")
+                continue
+            
+            start_seconds   = int(start_time[0]) * 3600 + int(start_time[1]) * 60 + int(start_time[2])
+            start_frame     = int(fps * start_seconds)
+
+            if total_length < start_frame:
+                print("잘못된 시간인거 같은디")
+                continue
+
+            inputTime   = input("끝 시간 입력 (ex: 00:12:53) >>>")
+            end_time    = timeSplitter.findall(inputTime)
+
+            if not len(end_time) == 3:
+                print("제대로 입력하셈 hh:mm:ss 맞춰야함")
+                continue
+
+            end_seconds = int(end_time[0]) * 3600 + int(end_time[1]) * 60 + int(end_time[2])
+            end_frame   = int(fps * end_seconds)
+
+            if total_length < end_frame:
+                print("잘못된 시간인거 같은디")
+                continue
+
+            if start_frame >= end_frame:
+                print("왜 시작 시간이 더 뒤에 있음;;;")
+                continue
+
+            part_length = end_frame - start_frame
+            accumulated_length += part_length
+
+            # 구간의 시작,끝 프레임과 길이를 저장
+            tmp_list = []
+            tmp_list.append(start_frame)
+            tmp_list.append(end_frame)
+            tmp_list.append(part_length)
+            range_list.append(tmp_list)
+
+            break
+
+    if accumulated_length < extraction_size:    print("너무 짧음")
+    else:   break
+
+# 랜덤으로 프레임 뽑기
+for iteration in range(random_limit):
+
+    while True:
+        random_index = rand(0,count-1)
+        random_frame = rand(range_list[random_index][0], range_list[random_index][1])
+
+        if random_frame % shot_length != 0  : continue
+        if random_frame in randFrame_list   : continue
+
+        else:
+            randFrame_list.append(random_frame)
+            break
+
+# 랜덤 프레임 정렬 여부
+while True:
+    print("\n랜덤으로 뽑은 프레임 정렬할까요? y 또는 n 입력")
+    select = input("선택 (ex: y)>>>")
+    if select == "y":
+        randFrame_list.sort()
+        break
+    if select == "n":
+        break
+
+# 영화 장르 이름으로 폴더 생성
+try:
+    if not(os.path.isdir(typeName)):
+        os.makedirs(os.path.join(typeName))
+except OSError as e:
+    if e.errno != OSError.errno.EEXIST:
+        raise
+os.chdir(typeName)
+
+# 영화 넘버링 확인
+for index in range(101):
+    if index == 0: continue
+    if os.path.exists(typeName + '_movie%d_1.png'%index): continue
+    else:
+        movie_num = index
+        break
+
+# 각 구간에서 뽑은 랜덤 프레임으로 이동하면서 연속된 샷의 프레임들을 뽑아서 저장
+
+prev_frame = np.zeros_like(frame, np.float32)
+for frame_num in randFrame_list:
+    for shot in range(shot_length):
+        vid.set(cv.CAP_PROP_POS_FRAMES, frame_num + shot)
+        flag, frame     = vid.read()
+
+        while True:
+            diff = (np.sum((frame - prev_frame)*(frame - prev_frame)) / (width*height*3))
+            if(diff > 100):
+                print(diff)
+                break
+            else:
+                flag, frame     = vid.read()
+        
+        resized_frame   = cv.resize(frame, (width,height))
+
+        cv.imshow(fileName, resized_frame)
+        cv.waitKey(10)
+        cv.imwrite(typeName + '_movie%d_%d.png'%(movie_num, file_num), resized_frame)
+
+        np.copyto(prev_frame, frame)
+        file_num += 1
+
+vid.release()
+cv.destroyAllWindows()
---
+++ tools/opticalflow.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+ 
+frame1 = cv2.imread('action_movie7_15.png')
+frame2 = cv2.imread('action_movie7_16.png')
+
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+
+width = frame1.shape[1]
+height = frame1.shape[0]
+
+flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+channel = flow.reshape(height,width, 2)
+
+max_val = channel.max()
+col_motion = channel[:,:,0]
+row_motion = channel[:,:,1]
+# 여기서 정의된 col_motion과 row_motion 채널을 사용하기
+# 모션을 쌓아서 돌릴 때 R, G, B, ROW_MOTION, COL_MOTION 이렇게 하면 
+# 아래는 볼 필요 없는 코드 (시각화)
+
+cv2.imshow('col motion',col_motion / max_val)
+cv2.imshow('row motion',row_motion / max_val)
+mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+hsv = np.zeros_like(frame1)
+hsv[...,0] = ang*180/np.pi/2
+hsv[...,1] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+hsv[...,2] = 255
+rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+cv2.imshow('visualized',rgb)
+cv2.waitKey()
+cv2.destroyAllWindows()
