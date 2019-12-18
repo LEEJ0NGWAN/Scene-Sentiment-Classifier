f7a7b3ff20dc2e5edd13c198d1fefda1948abc55
Lee Jong Wan <lastffx@gmail.com>
2019-12-19 01:04:49 +0900

    fix little

--- lib/moception3.py
+++ lib/moception3.py
@@ -101,8 +101,9 @@ class Moception(nn.Module):
             self.AuxLogits = inception_aux(768, num_classes)
         self.Mixed_7a = inception_d(768)
         self.Mixed_7b = inception_e(1280)
-        self.Mixed_7c = inception_e(2048)
-        self.fc = nn.Linear(2048 + 1, num_classes)
+        self.Mixed_7c = inception_e(2048)        
+        # self.fc = nn.Linear(2048 + 1, num_classes)
+        self.fc = nn.Linear(2048, num_classes)
         self.motion_fc = nn.Linear(1,1)
 
         for m in self.modules():
@@ -235,8 +236,8 @@ class Moception(nn.Module):
         x2 = motion_batch
         x2 = self._transform_motion_input(x2)
         x2 = self.motion_fc(x2)
-        x = torch.cat([x1, x2], 1) # 2048+1
-        x = self.fc(x) # 2049 -> classnum
+        # x = torch.cat([x1, x2], 1) # 2048+1
+        x = self.fc(x1) # 2049 -> classnum
 
         aux_defined = False #self.training and self.aux_logits
         if torch.jit.is_scripting():
